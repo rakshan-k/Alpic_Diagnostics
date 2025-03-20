be9013e064f0677e03f9f5263f96af444b58a0e1
root <root@kali.zeus>
2025-03-20 14:51:22 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react()],  // Remove this if not using React
+  plugins: [react()],
   server: {
-    port: 3000  // Change this if needed
-  }
+    historyApiFallback: true,
+    port: 3000
+  },
 });
+
